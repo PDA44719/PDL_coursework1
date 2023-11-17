f518b2966f1315138e11f45deae32beda1f3e8ab
PDA44719 <zceepde@ucl.ac.uk>
2023-11-17 13:51:49 +0000

    Reformatted PrimaryMarket and got rid of unnecessary comments.

--- src/contracts/PrimaryMarket.sol
+++ src/contracts/PrimaryMarket.sol
@@ -4,75 +4,72 @@ import "../interfaces/IPrimaryMarket.sol";
 import "../contracts/PurchaseToken.sol";
 import "../contracts/TicketNFT.sol";
 
-contract PrimaryMarket is IPrimaryMarket{ //is IPrimaryMarket to be added
-	/* This section is to be deleted before submission, as the interface 
-	already has these events, so there is no need to add them here*/
+contract PrimaryMarket is IPrimaryMarket {
+    event Log(address someone); // TO BE DELETED BEFORE SUBMITTING
 
-    event Log(address someone);
-	/* End of Section to be Deleted */
+    PurchaseToken _purchaseToken;
+    mapping(address => uint256) internal _priceOfATicket;
 
-	PurchaseToken _purchaseToken;
-	mapping(address => uint256) internal priceOfATicket;
-
-	constructor(PurchaseToken purchaseToken){
-		_purchaseToken = purchaseToken;
-	}
+    constructor(PurchaseToken purchaseToken) {
+        _purchaseToken = purchaseToken;
+    }
 
     function createNewEvent(
         string memory eventName,
         uint256 price,
         uint256 maxNumberOfTickets
-    ) external returns (ITicketNFT ticketCollection){ 
-
-		/*bytes memory callData = abi.encodePacked(
-            type(TicketNFT).creationCode,
-            abi.encode(eventName, msg.sender)
+    ) external returns (ITicketNFT ticketCollection) {
+        // Create collection and define the ticket price
+        TicketNFT collection = new TicketNFT(
+            eventName,
+            maxNumberOfTickets,
+            msg.sender
         );
-		
-		address newTicketNFT;
-		assembly {
-            newTicketNFT := create(0, add(callData, 0x20), mload(callData))
-            if iszero(extcodesize(newTicketNFT)) {
-                revert(0, 0)
-            }
-        }
-		return TicketNFT(newTicketNFT);
-		*/
-		
-		TicketNFT collection = new TicketNFT(
-			eventName,
-			maxNumberOfTickets,
-			msg.sender
-		);
-		priceOfATicket[address(collection)] = price;
-		emit EventCreated(msg.sender, address(collection), eventName, 
-						 price, maxNumberOfTickets);
-		return TicketNFT(collection);
-		 
-	}
+        _priceOfATicket[address(collection)] = price;
+
+        emit EventCreated(
+            msg.sender,
+            address(collection),
+            eventName,
+            price,
+            maxNumberOfTickets
+        );
+        return TicketNFT(collection);
+    }
 
     function getPrice(
         address ticketCollection
-    ) external view returns (uint256 price){
-		return priceOfATicket[ticketCollection];
-	}
+    ) external view returns (uint256 price) {
+        return _priceOfATicket[ticketCollection];
+    }
 
     function purchase(
         address ticketCollection,
         string memory holderName
-    ) external returns (uint256 id){
+    ) external returns (uint256 id) {
         TicketNFT collection = TicketNFT(ticketCollection);
-        // Check that more tickets can be minted
-        require(collection.getNumberOfMinted() < collection.maxNumberOfTickets(), "No more tickets can be minted");
 
-        // Check that the buyer has enough funds and has approved the amount
-        require(_purchaseToken.allowance(msg.sender, address(this)) >= priceOfATicket[ticketCollection], "Ticket price was not approved before purchase");
-        require(_purchaseToken.balanceOf(msg.sender) >= priceOfATicket[ticketCollection], "Insufficient funds");
+        // Ensure that sender has approved the amount and has sufficient funds
+        require(
+            _purchaseToken.allowance(msg.sender, address(this)) >=
+                _priceOfATicket[ticketCollection],
+            "Ticket price was not approved before purchase"
+        );
+        require(
+            _purchaseToken.balanceOf(msg.sender) >=
+                _priceOfATicket[ticketCollection],
+            "Insufficient funds"
+        );
 
+        // Mint ticket and transfer purchase price to the collection creator
         uint256 newTokenID = collection.mint(msg.sender, holderName);
+        _purchaseToken.transferFrom(
+            msg.sender,
+            collection.creator(),
+            _priceOfATicket[ticketCollection]
+        );
+
         emit Purchase(msg.sender, ticketCollection, newTokenID, holderName);
-        _purchaseToken.transferFrom(msg.sender, collection.creator(), priceOfATicket[ticketCollection]);
         return newTokenID;
     }
-
 }
